42416a639000d5e0462a6d9d8fdcb4c315698e49
bnosoft2024 <bnojunior2024@gmail.com>
2025-11-10 13:02:07 +0900

    검색추가수정

--- vueNuxtBoardFront/components/board/BoardSearch.vue
+++ vueNuxtBoardFront/components/board/BoardSearch.vue
@@ -1,26 +1,21 @@
 <template>
     <div class="board-search">
         <!-- 검색어 입력 -->
-        <input v-model="localKeyword"
-        type="text"
-        placeholder="제목 검색..."
-        class="search-input" 
-        @input="$emit('update:searchKeyword', localKeyword)"
-         />
+        <input v-model="localKeyword" type="text" placeholder="제목 검색..." class="search-input"
+            @input="$emit('update:searchKeyword', localKeyword)" />
 
         <!-- 검색 결과 -->
         <!-- filteredBoards.length>0 : true  => 렌더링됨
             filteredBoards.length==0 : false => 검색어가 null, 또는 비어있는 경우 렌더링되지 않음  
         -->
         <!-- @select 이벤트: 검색 결과 중 하나를 클릭 시 발생하는 ui 이벤트 => 여기서는 상세페이지 이동 효과 -->
-        <div v-if="filteredBoards.length" class="search-results"> 
+        <div v-if="filteredBoards.length" class="search-results">
             <ul>
                 <li v-for="board in filteredBoards" :key="board.boardId">
                     <span class="result-title" @click="$emit('select', board)">
-                        {{ board.title }}
+                        <a :href="`http://localhost:3000/board/detail?id=${board.boardId}`">{{ board.title }}</a>
                     </span>
                     <small> - {{ board.writerId }}</small>
-                    <a :href="`http://localhost:3000/board/detail?id=${board.boardId}`">{{board.boardId}}</a>
                 </li>
             </ul>
         </div>
@@ -42,18 +37,18 @@ const props = defineProps({
 
 
 // 로컬 상태(입력용)
-const localKeyword=ref(props.searchKeyword || "")
+const localKeyword = ref(props.searchKeyword || "")
 
 // BoardSidebar(상위 컴포넌트)로 update, select 이벤트 전달
 const emit = defineEmits(["update:searchKeyword", "select"])
 
 // props 값이 외부에서 바뀌면 입력창도 동기화
 watch(
-  () => props.searchKeyword,
-  (newVal) => {
-    localKeyword.value = newVal;
-    console.log("props: ",props.filteredBoards)
-  }
+    () => props.searchKeyword,
+    (newVal) => {
+        localKeyword.value = newVal;
+        console.log("props: ", props.filteredBoards)
+    }
 );
 </script>
 
